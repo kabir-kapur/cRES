4e691ecf453d0a523f18269e288bea302272a3bb
Kabir Kapur <krkapur@ucsc.edu>
2021-07-16 15:03:29 -0700

    init

---
+++ contracts/cResToken.sol
@@ -0,0 +1,12 @@
+pragma solidity ^0.5.16;
+
+contract cResToken{
+    /*
+    NEED TO MAKE:
+    - constructor
+    - total # of tokens setter
+    - total # of tokens reader */
+    function supply() public {
+        uint256 totalSupply = 1000000;
+    }
+}
