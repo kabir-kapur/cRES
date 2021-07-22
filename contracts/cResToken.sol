076dddbedfcdc23762ba08fc093e6e13afb17ae4
Kabir Kapur <krkapur@ucsc.edu>
2021-07-22 15:50:50 -0700

    tests -- added cResToken spotprice test

--- contracts/cResToken.sol
+++ contracts/cResToken.sol
@@ -2,35 +2,6 @@ pragma solidity ^0.6.0;
 
 import "@chainlink/contracts/src/v0.6/ChainlinkClient.sol"; // importing v0.6 (stable)
 
-contract BTCAPIConsumer is ChainlinkClient{ 
-    /*
-    parent: ChainlinkClient
-    */
-    uint256 public btcPrice; // price of btc
-
-    address private oracle;
-    bytes32 private jobId;
-    uint256 private fee;
-
-    constructor() public{
-        setPublicChainlinkToken();
-
-        oracle = 0xECcB8F881cE2552EdA4115a162ffE2666B601c33; // https://market.link/data-providers/eb5c92a8-6093-4657-9a68-a6d10719946e/integrations?network=1
-        jobId = "0391a670ba8e4a2f80750acfe65b0c89";
-        fee = .1 * 10**18; // fee of .1 LINK
-    }
-
-    function requestBTCPrice() public returns (bytes32 reqID){
-        Chainlink.Request memory req = buildChainlinkRequest(jobId, address(this), this.fulfillBTC.selector);
-        req.add("base", "BTC/USDT:CXDXF");
-        
-        return sendChainlinkRequestTo(oracle, req, fee);
-    }
-
-    function fulfillBTC(bytes32 _requestId, uint256 _btcPrice) public recordChainlinkFulfillment(_requestId){
-        btcPrice = _btcPrice;
-    }
-}
 contract cResToken{
     /*
     NEED TO MAKE:
@@ -42,11 +13,51 @@ contract cResToken{
         events undergone as of current epoch 't'))  
     - V = product over t tokens((TOKEN tokens/POOL tokens)^(1/t))
     - */
-    function valueAsset() private{
-        return
+
+    uint _totalSupply = 10000;
+
+
+    function spotPrice(uint256 celoRatio, uint256 celoInReserve, uint256 totalInReserve) view public returns(uint256 spotPricecRES){
+        return ((celoRatio/100)/celoInReserve)/((1 - celoRatio)/(totalInReserve - celoInReserve));
     }
 
-    function supply() public {
-        uint256 totalSupply = 1000000;
+    function supply() view public returns(uint _totalSupply){
+        return _totalSupply;
     }
 }
+
+
+
+////////////////// DATA FEED STUFF //////////////////
+
+
+
+// contract BTCAPIConsumer is ChainlinkClient{ 
+//     /*
+//     parent: ChainlinkClient
+//     */
+//     uint256 public btcPrice; // price of btc
+
+//     address private oracle;
+//     bytes32 private jobId;
+//     uint256 private fee;
+
+//     constructor() public{
+//         setPublicChainlinkToken();
+
+//         oracle = 0xECcB8F881cE2552EdA4115a162ffE2666B601c33; // https://market.link/data-providers/eb5c92a8-6093-4657-9a68-a6d10719946e/integrations?network=1
+//         jobId = "0391a670ba8e4a2f80750acfe65b0c89";
+//         fee = .1 * 10**18; // fee of .1 LINK
+//     }
+
+//     function requestBTCPrice() public returns (bytes32 reqID){
+//         Chainlink.Request memory req = buildChainlinkRequest(jobId, address(this), this.fulfillBTC.selector);
+//         req.add("base", "BTC/USDT:CXDXF");
+        
+//         return sendChainlinkRequestTo(oracle, req, fee);
+//     }
+
+//     function fulfillBTC(bytes32 _requestId, uint256 _btcPrice) public recordChainlinkFulfillment(_requestId){
+//         btcPrice = _btcPrice;
+//     }
+// }
--- test/cResToken_test.js
+++ test/cResToken_test.js
@@ -1,7 +1,16 @@
 const cResToken = artifacts.require("cResToken");
 
 contract("cResToken", (accounts) => {
-    it("Test if we can correctly get the price of BTC", () => {
+    it("Test spot price mechanism", () => {
+        cResToken.deployed()
+        .then(instance => instance.spotPrice.call(10, 10000000, 15000000))
+        .then(price => {
+            assert.equal(
+                spotPrice.valueOf(),
+                1.5,
+                "spot price innacurate"
+            )
+        })
     })
 })
    
