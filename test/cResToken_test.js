3af0b87909e998ceb6260893b70bf72cd72c12c3
Kabir Kapur <krkapur@ucsc.edu>
2021-07-23 18:52:26 -0700

    feature -- attempting to set up alfajores config

--- test/cResToken_test.js
+++ test/cResToken_test.js
@@ -6,21 +6,27 @@ contract("cResToken", accounts => { // contract instantiation
         const cResTokenInstance = await cResToken.deployed(); // create deployed() promise
         const supply = await cResTokenInstance.supply.call(); // call supply() 
 
-        console.log(supply.valueOf());
         assert.equal(supply.valueOf(), 10000, "Supply is not 10000"); // assert supply == correct val
     });
 
     it("Test spotPrice()", async() => {
-        const cResTokenInstance = await cResToken.deployed();
-        const spotPrice = await cResTokenInstance.spotPrice.call(50, 10000000, 15000000);
+        // modify inputs to test varying environment
+        const cRatioIn = 10;
+        const cWeightIn = 100000000;
+        const rWeightIn = 15000000;
 
-        console.log(spotPrice.valueOf());
-        let cRatio = spotPrice.valueOf()[0]/100;
-        let rRatio = 1 - cRatio;
-        let cWeight = spotPrice.valueOf()[1];
-        let rWeight = spotPrice.valueOf()[2];
-        let spotPriceVal = (cRatio/cWeight) / (rRatio/rWeight);
-        assert.equal(spotPriceVal, 1.5, "Spot Price is not 1.5");
+        const targetVal = ((cRatioIn/100)/cWeightIn) / (((100 - cRatioIn)/100)/rWeightIn);
+
+        const cResTokenInstance = await cResToken.deployed();
+        const spotPriceMetrics = await cResTokenInstance.spotPrice.call(cRatioIn, cWeightIn, rWeightIn);
+
+        let cRatioOut = spotPriceMetrics.valueOf()[0]/100; // declare valuation variables
+        let rRatioOut = 1 - cRatioOut;
+        let cWeightOut = spotPriceMetrics.valueOf()[1];
+        let rWeightOut = spotPriceMetrics.valueOf()[2];
+        let spotPriceVal = (cRatioOut/cWeightOut) / (rRatioOut/rWeightOut);
+         
+        assert.equal(spotPriceVal, targetVal, "spot price received does not match target"); // perform test
     })
 });
    
--- truffle-config.js
+++ truffle-config.js
@@ -23,6 +23,22 @@
 //
 // const fs = require('fs');
 // const mnemonic = fs.readFileSync(".secret").toString().trim();
+ 
+
+/*
+celo config
+ */ 
+const Web3 = require('web3')
+const ContractKit = require('@celo/contractkit')
+const web3 = new Web3('https://alfajores-forno.celo-testnet.org')
+const kit = ContractKit.newKitFromWeb3(web3)
+const getAccount = require('./getAccount').getAccount
+
+async function awaitWrapper(){
+    let account = await getAccount()
+    kit.connection.addAccount(account.privateKey)
+}
+awaitWrapper()
 
 module.exports = {
   /**
@@ -76,6 +92,10 @@ module.exports = {
       host: "127.0.0.1",
       port: "7545",
       network_id: "*"
+    },
+    alfajores: {
+      provider: kit.connection.web3.currentProvider,
+      network_id: 44787
     }
   },
 
