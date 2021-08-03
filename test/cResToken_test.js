b584965fbac837dc2a25971359ee81d511429067
Kabir Kapur <krkapur@ucsc.edu>
2021-08-02 17:57:50 -0700

    override existing supply, name, and symbol functions

--- test/cResToken_test.js
+++ test/cResToken_test.js
@@ -4,29 +4,43 @@ const cResToken = artifacts.require("cResToken")
 contract("cResToken", accounts => { // contract instantiation
     it("Test supply()", async() => { // give this test a name
         const cResTokenInstance = await cResToken.deployed(); // create deployed() promise
-        const supply = await cResTokenInstance.supply.call(); // call supply() 
+        const supply = await cResTokenInstance.totalSupply.call(); // call supply() 
 
         assert.equal(supply.valueOf(), 10000, "Supply is not 10000"); // assert supply == correct val
     });
 
-    it("Test spotPrice()", async() => {
-        // modify inputs to test varying environment
-        const cRatioIn = 10;
-        const cWeightIn = 100000000;
-        const rWeightIn = 15000000;
-
-        const targetVal = ((cRatioIn/100)/cWeightIn) / (((100 - cRatioIn)/100)/rWeightIn);
-
+    it("Test name()", async() => {
         const cResTokenInstance = await cResToken.deployed();
-        const spotPriceMetrics = await cResTokenInstance.spotPrice.call(cRatioIn, cWeightIn, rWeightIn);
+        const instName = await cResTokenInstance.name.call();
 
-        let cRatioOut = spotPriceMetrics.valueOf()[0]/100; // declare valuation variables
-        let rRatioOut = 1 - cRatioOut;
-        let cWeightOut = spotPriceMetrics.valueOf()[1];
-        let rWeightOut = spotPriceMetrics.valueOf()[2];
-        let spotPriceVal = (cRatioOut/cWeightOut) / (rRatioOut/rWeightOut);
+        assert.equal(instName, "Celo Reserve Token", "Name is not correct");
+    });
+
+    it("Test symbol()", async() => {
+        const cResTokenInstance = await cResToken.deployed();
+        const instSymbol = await cResTokenInstance.symbol.call();
+
+        assert.equal(instSymbol, "cRES", "Symbol is not correct");
+    });
+
+    // it("Test spotPrice()", async() => {
+    //     // modify inputs to test varying environment
+    //     const cRatioIn = 10;
+    //     const cWeightIn = 100000000;
+    //     const rWeightIn = 15000000;
+
+    //     const targetVal = ((cRatioIn/100)/cWeightIn) / (((100 - cRatioIn)/100)/rWeightIn);
+
+    //     const cResTokenInstance = await cResToken.deployed();
+    //     const spotPriceMetrics = await cResTokenInstance.spotPrice.call(cRatioIn, cWeightIn, rWeightIn);
+
+    //     let cRatioOut = spotPriceMetrics.valueOf()[0]/100; // declare valuation variables
+    //     let rRatioOut = 1 - cRatioOut;
+    //     let cWeightOut = spotPriceMetrics.valueOf()[1];
+    //     let rWeightOut = spotPriceMetrics.valueOf()[2];
+    //     let spotPriceVal = (cRatioOut/cWeightOut) / (rRatioOut/rWeightOut);
          
-        assert.equal(spotPriceVal, targetVal, "spot price received does not match target"); // perform test
-    })
+    //     assert.equal(spotPriceVal, targetVal, "spot price received does not match target"); // perform test
+    // })
 });
    
