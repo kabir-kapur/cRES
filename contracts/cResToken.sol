7fae4855f39c816e8a9f717dc32b1bfcc7d448f4
Kabir Kapur <kapka6700@gmail.com>
2022-01-15 16:40:13 -0800

    super greaze

--- contracts/cResToken.sol
+++ contracts/cResToken.sol
@@ -1,47 +1,78 @@
-pragma solidity ^0.8.0;
+pragma solidity ^0.8.7;
 
 // SPDX-License-Identifier: <SPDX-License>
 
 // hash: 0x3591A63b1986AE4080a196DF6C1b20A07c34678f
 // 0x3591A63b1986AE4080a196DF6C1b20A07c34678f
 
-// mnemonic:
-/*rapid exhibit minor hero staff want shed 
- stock just glimpse vapor discover brown 
- fitness omit slush entry print advice antenna
- senior gate slim carbon */
+/*
+   mnemonic:
+   rapid exhibit minor hero staff want shed
+   stock just glimpse vapor discover brown
+   fitness omit slush entry print advice antenna
+   senior gate slim carbon
+*/
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
-import "./DIAOracle.sol";
+import "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
 
-
+contract DIAOracleV2 {
+    mapping (string => uint256) public values;
+    address oracleUpdater;
+    
+    event OracleUpdate(string key, uint128 value, uint128 timestamp);
+    event UpdaterAddressChange(address newUpdater);
+    
+    constructor() {
+        oracleUpdater = msg.sender;
+    }
+    
+    function setValue(string memory key, uint128 value, uint128 timestamp) public {
+        require(msg.sender == oracleUpdater);
+        uint256 cValue = (((uint256)(value)) << 128) + timestamp;
+        values[key] = cValue;
+        emit OracleUpdate(key, value, timestamp);
+    }
+    
+    function getValue(string memory key) external view returns (uint128, uint128) {
+        uint256 cValue = values[key];
+        uint128 timestamp = (uint128)(cValue % 2**128);
+        uint128 value = (uint128)(cValue >> 128);
+        return (value, timestamp);
+    }
+    
+    function updateOracleUpdaterAddress(address newOracleUpdaterAddress) public {
+        require(msg.sender == oracleUpdater);
+        oracleUpdater = newOracleUpdaterAddress;
+        emit UpdaterAddressChange(newOracleUpdaterAddress);
+    }
+}
 
 contract cRESToken is ERC20, ERC20Burnable{
     uint256 currentPrice = 1;
-    uint256 mostRecentDeposit; 
-    uint256 btcPrice = 45000;
-    uint256 celoPrice = 3;
-    uint256 ethPrice = 3500;
+    uint256 mostRecentDeposit;
+    uint256 btcPrice = 69000;
+    uint256 celoPrice = 6;
+    uint256 ethPrice = 4000;
     uint256 stablePrice = 1;
 
-    DiaOracle oracle;
-
     struct expectedRatios{
-        uint BTC;
-        uint CELO;
-        uint cUSD;
-        uint ETH;
+        uint256 BTC;
+        uint256 CELO;
+        uint256 cUSD;
+        uint256 ETH;
     }
 
     struct purchaseAmounts{
-        uint BTC;
-        uint CELO;
-        uint cUSD;
-        uint ETH;
+        uint256 BTC;
+        uint256 CELO;
+        uint256 cUSD;
+        uint256 ETH;
     }
 
     purchaseAmounts amounts;
+    AggregatorV3Interface internal priceFeed; // state var priceFeed
 
     IERC20 public cRES;
     expectedRatios ratios = expectedRatios({BTC:30, CELO:50, cUSD:5, ETH:15});
@@ -49,40 +80,38 @@ contract cRESToken is ERC20, ERC20Burnable{
     mapping(address=>uint) balances;
 
     event Bought(uint256);
+    event dbPrice(uint256);
+    event PricePair(uint128, uint128);
+    event uPrice(uint256);
 
-    constructor() ERC20("Celo Reserve Token", "cRES"){ // mint 1 token to a
-        _mint(address(this), 1); 
+    constructor() ERC20("Celo Reserve Coin", "cRES"){ // mint 1 token to this
+        _mint(address(this), 1);
         balances[address(this)] += 1;
-        oracle = DiaOracle(0xb73db1A6a85219742fbd0fC7cc275c62209aA660);
+        priceFeed = AggregatorV3Interface(0xF4030086522a5bEEa4988F8cA5B36dbC97BeE88c); // assign
     }
 
     function mint(uint256 amount) public {
         _mint(address(this), amount);
     }
 
-    function sendcRES(uint256 _amount) external payable { 
-        _mint(address(this), _amount); // mint AMOUNT tokens
+    function sendcRES(uint256 _amount) external payable {
+        _mint(address(this), _amount);
         assert(_amount <= cRES.balanceOf(address(this)));
-        assert(_amount >= currentPrice);
-        transfer(msg.sender, _amount); // transfer AMOUNT worth of cRES
+        // assert(_amount >= currentPrice);
+        payable(msg.sender).transfer(_amount);
         emit Bought(_amount);
     }
 
     function receivecUSD(uint256 _amount) external payable { // receive deposit
-        transfer(address(this), _amount);
+        payable(address(this)).transfer(_amount);
         mostRecentDeposit = _amount;
         emit Transfer(address(this), msg.sender, _amount);
     }
 
-    function getBTC() external view returns(uint) { // pull btc price feed
-        // DiaOracle oracle = DiaOracle(0xCd8E18890E416Aa7ab09aa793b406C187747C687); // declare oracle instance @ celo oracle location
-        // DiaOracle oracle = DiaOracle(0xb73db1A6a85219742fbd0fC7cc275c62209aA660); // declare oracle instance @ kovan oracle location
-        uint256 price;
-        uint256 supply;
-        uint256 lastUpdateTimestamp;
-        string memory symbol;
-        (price, supply, lastUpdateTimestamp, symbol) = oracle.getCoinInfo("Bitcoin");
-        return price;
+    function getBTC() public { // pucalll btc price feed
+        DIAOracleV2 dia = DIAOracleV2(0xCd8E18890E416Aa7ab09aa793b406C187747C687);
+        (uint128 key, uint128 value) = dia.getValue("Bitcoin");
+        emit PricePair(key, value);
     }
 
     function getETH() private returns(uint) { // pull eth price feed
@@ -113,17 +142,17 @@ contract cRESToken is ERC20, ERC20Burnable{
         return total;
     }
 
-    function spotPriceHard() public pure returns(uint256){ // fully hard-coded values sheeeesh
+    function spotPriceHard() private pure returns(uint256){ // fully hard-coded values sheeeesh
         // uint256 btcAmt = 1;
         // uint256 celoAmt = 1000;
         // uint256 ethAmt = 10;
         // uint256 daiAmt = 50;
 
         // uint256 total = btcAmt + celoAmt + ethAmt + daiAmt;
-        
+
         uint256 ret = 1061;
 
         return ret;
     }
-    
 }
+
